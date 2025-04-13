759ff9c685bc2faa0168d1786f44bf10d3c73da5
revsj <rexjalandoni08@gmail.com>
2025-04-14 00:50:23 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+    document.body.classList.toggle('light-theme');
+
+    const className = document.body.className;
+    if (className === 'light-theme') {
+        this.textContent = "dark";
+    } else {
+        this.textContent = "light";
+    }
+
+    console.log ( 'current theme ' + className );
+});
